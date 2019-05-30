95b06dd6efe053e58be69365a23d0b02521ba0f2
pangdabo <pang2081@163.com>
2019-05-30 19:01:54 +0800

    11

--- java8/src/main/java/example/collections/CollectionsSort.java
+++ java8/src/main/java/example/collections/CollectionsSort.java
@@ -10,29 +10,28 @@ import java.util.List;
 public class CollectionsSort {
 
     public static void main(String[] args) {
-        List<Character> list = Arrays.asList('a', 'c', 'b', 'f', 'e','d');
+        List<Character> list = Arrays.asList('a', 'c', 'b', 'f', 'e', 'd');
 
 
         // 排序
         // 如果同时有自然排序和比较器排序，以比较器排序为主
 
         // 自然排序
-        /*Collections.sort(list);*/
+//        Collections.sort(list);
 
         // 比较器排序  使用匿名类 为 Collections.sort() 定制 Comparator。
-        /*
-        Collections.sort(list, new Comparator<Character>() {
-            @Override
-            public int compare(Character c1, Character c2) {
-                return c1-c2;
-            }
-        });
-        */
+//        Collections.sort(list, new Comparator<Character>() {
+//            @Override
+//            public int compare(Character c1, Character c2) {
+//                return c1-c2;
+//            }
+//        });
 
 
         // Lambda
-        Collections.sort(list, (c1, c2) -> c1 - c2);
-
+//         Collections.sort(list, (c1, c2) -> c1 - c2);
+//        list.sort(Comparator.comparingInt(c -> c));
+//        list.sort(null);
 
         // 遍历集合
         /*for (Character s : list) {
